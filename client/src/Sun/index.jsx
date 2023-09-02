eba41d0b5a3f5ed2ee5c5fb48dd427f051af0262
moon-rhino <107901354+Jo-Moonsoo@users.noreply.github.com>
2023-09-02 09:34:32 -0700

    sun sandbox for Bagus

--- client/src/Sun/index.jsx
+++ client/src/Sun/index.jsx
@@ -4,7 +4,7 @@ import { Canvas } from "@react-three/fiber";
 // import Experience from "./ExperienceBackup";
 import Experience from "./Experience";
 import MouseGame from "./minigames/MouseGame";
-import Sandbox from "./Sandbox"
+import Sandbox from "./Sandbox";
 import * as THREE from "three";
 import {
   CameraControls,
@@ -26,26 +26,22 @@ import { SocketManager } from "./SocketManager";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <KeyboardControls map={[{ name: "throw", keys: ["Space"] }]}>
-    <SocketManager />
-    <Canvas
-      gl={{
-        antialias: true,
-        toneMapping: THREE.ACESFilmicToneMapping,
-        outputEncoding: THREE.sRGBEncoding,
-      }}
-      // camera={{ fov: 45, near: 0.1, far: 500, position: [6, 6, 6] }}
-      // pan: move about a plane
-    >
-      {/* <CameraControls /> */}
-      <OrbitControls />
+  <Canvas
+    gl={{
+      antialias: true,
+      toneMapping: THREE.ACESFilmicToneMapping,
+      outputEncoding: THREE.sRGBEncoding,
+    }}
+    // camera={{ fov: 45, near: 0.1, far: 500, position: [6, 6, 6] }}
+    // pan: move about a plane
+  >
+    {/* <CameraControls /> */}
+    <OrbitControls />
 
-      {/* <Experience /> */}
-      <Sandbox/>
-      {/* <MouseGame /> */}
+    {/* <Experience /> */}
+    <Sandbox />
+    {/* <MouseGame /> */}
 
-      {/* <Perf /> */}
-    </Canvas>
-    {/* <Controls /> */}
-  </KeyboardControls>
+    {/* <Perf /> */}
+  </Canvas>
 );
