b0dcfccffdd426df62b1db3662d753763c2fa83e
moonsoojo <jojuno@ucla.edu>
2023-10-16 17:48:48 -0700

    throw result test pseudocode

---
+++ client/src/observeThrowResult.js
@@ -0,0 +1,8 @@
+// test
+// a set of 2 angles per result each
+// you should know the result beforehand
+
+// another test
+// set yuts at specified locations
+// throw them with specified force
+// when they rest, they should give the same result for 150 throws
