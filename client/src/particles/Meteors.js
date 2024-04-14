8f29cb8f30271cf2d67a97525b9645251364107d
moon-rhino <107901354+Jo-Moonsoo@users.noreply.github.com>
2024-04-14 09:28:49 -0700

    revert adding radial velocity to meteors

--- client/src/particles/Meteors.js
+++ client/src/particles/Meteors.js
@@ -37,16 +37,15 @@ export default function meteorSettings(device) {
                     new Position(zone),
                     new Mass(0.1),
                     new Radius(1.5, 2),
-                    new Life(3, 4),
-                    new Body(createSprite('./textures/dot.png')),
-                    new RadialVelocity(0.2, new Vector3D(0, 10, 0), 500, true)
+                    new Life(1.5, 2),
+                    new Body(createSprite('./textures/dot.png'))
                 ],
                 behaviours: [
                     new Alpha(0.7, 0),
                     new Scale(0.6, 0.4),
                     new Color(new THREE.Color(colors.getValue()), new THREE.Color(colors.getValue())),
                 ],
-                numEmit: 10,
+                numEmit: 8,
                 moving: true
             }
         )
