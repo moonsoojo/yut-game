c7cb4f69a636d06692f03da5abea16828b844abc
moon-rhino <107901354+Jo-Moonsoo@users.noreply.github.com>
2024-04-21 16:58:04 -0700

    reuse Board component in Home

--- client/src/Home2.jsx
+++ client/src/Home2.jsx
@@ -24,6 +24,7 @@ import Stars from './particles/Stars';
 import { socket } from './SocketManager';
 import { clientAtom, deviceAtom } from './GlobalState';
 import mediaValues from './mediaValues';
+import Board from './Board';
 
 export default function Home2() {
 
@@ -34,113 +35,6 @@ export default function Home2() {
   const { scene, materials } = useGLTF(
     "models/yoot.glb"
   );
-
-  const TILE_RADIUS = layout[device].tileRadius.ring;
-  const NUM_STARS = 20;
-  function Tiles() {
-    let tiles = [];
-
-    //circle
-    for (let i = 0; i < NUM_STARS; i++) {
-      let position = [
-        -Math.cos(((i+5) * (Math.PI * 2)) / NUM_STARS) * TILE_RADIUS,
-        0,
-        Math.sin(((i+5) * (Math.PI * 2)) / NUM_STARS) * TILE_RADIUS,
-      ];
-      if (i == 0) {
-        tiles.push(<Earth position={position} tile={i} key={i} device={device}/>);
-      } else if (i == 5) {
-        tiles.push(
-          <Mars
-            position={position}
-            tile={i}
-            key={i}
-            device={device}
-          />
-        );
-      } else if (i == 10) {
-        tiles.push(<Saturn position={position} tile={i} key={i} device={device}/>);
-      } else if (i == 15) {
-        tiles.push(<Neptune position={position} tile={i} key={i} device={device}/>);
-      } else {
-        tiles.push(
-          <Star
-            position={position}
-            tile={i}
-            key={i}
-            scale={layout[device].star.scale}
-            device={device}
-          />
-        );
-      }
-    }
-
-    //shortcuts
-    const radiusShortcut1 = layout[device].tileRadius.shortcut1;
-    const radiusShortcut2 = layout[device].tileRadius.shortcut2;
-    for (let i = 0; i < NUM_STARS; i++) {
-      let indexShortcut1;
-      let indexShortcut2;
-      if (i == 0) {
-        indexShortcut1 = 24;
-        indexShortcut2 = 23;
-      } else if (i == 5) {
-        indexShortcut1 = 28;
-        indexShortcut2 = 27;
-      } else if (i == 10) {
-        indexShortcut1 = 20;
-        indexShortcut2 = 21;
-      } else if (i == 15) {
-        indexShortcut1 = 25;
-        indexShortcut2 = 26;
-      }
-      if (i == 0 || i == 5 || i == 10 || i == 15) {
-        let position1 = [
-          Math.sin(((i -5) * (Math.PI * 2)) / NUM_STARS) *
-            radiusShortcut1,
-          0,
-          Math.cos(((i -5) * (Math.PI * 2)) / NUM_STARS) *
-            radiusShortcut1,
-        ]
-        tiles.push(
-          <Star
-            position={position1}
-            tile={indexShortcut1}
-            key={i + 30}
-            scale={layout[device].star.scale}
-            device={device}
-          />
-        );
-        let position2 = [
-          Math.sin(((i -5) * (Math.PI * 2)) / NUM_STARS) *
-            radiusShortcut2,
-          0,
-          Math.cos(((i -5) * (Math.PI * 2)) / NUM_STARS) *
-            radiusShortcut2,
-        ]
-        tiles.push(
-          <Star
-            position={position2}
-            tile={indexShortcut2}
-            key={i + 41}
-            scale={layout[device].star.scale}
-            device={device}
-          />
-        );
-      }
-    }
-    // center piece
-    tiles.push(
-      <Moon
-        position={[0,0,0]}
-        intensity={3}
-        key={100}
-        tile={22}
-        device={device}
-      />
-    );
-    return tiles;
-  }
 
   const { ufoRotation, ufoPosition } = useSpring({
     from: {
@@ -305,7 +199,7 @@ export default function Home2() {
       >
         { display === 'board' && <group 
         >
-          <Tiles/>
+          <Board/>
           <Pieces/>
         </group>}
       </group>
